261aa0a47848384db3f823ce9bde1e095c6b8c66
Eric Walz <walzer85@gmail.com>
2018-07-18 22:39:48 -0500

    about to install mdbootstrap and want a better point to restore if I screw up.  Also making a branch

---
+++ cat-house-percussion/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Carousel } from 'react-bootstrap';
+import { Row } from 'react-flexbox-grid';
+import '../App.css'
+
+class Footer extends Component {
+  render() {
+    return(
+      <div>
+        
+      </div>
+    )
+  }
+}
