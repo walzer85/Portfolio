98bf3bc7dd90e6395786842c106dd39a3c648bd2
Eric Walz <walzer85@gmail.com>
2018-07-21 11:37:49 -0500

    starting to add react router, still need to add a footer, more media, and testimonials

--- cat-house-percussion/src/App.js
+++ cat-house-percussion/src/App.js
@@ -3,22 +3,23 @@ import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
 import Media from './components/Media';
+import {
+  BrowserRouter,
+  Route,
+  Switch
+ } from "react-router-dom";
+ 
 
 class App extends Component {
   render() {
     return (
+      <BrowserRouter>
       <div className="App">
         <Header />
         <Home />
         <Media />
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header> */}
-        {/* <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p> */}
       </div>
+      </BrowserRouter>
     );
   }
 }
