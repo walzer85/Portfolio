9e72c4736bd15aa1be69615ac3057ab647f37a9f
Eric Walz <walzer85@gmail.com>
2018-07-21 12:09:25 -0500

    added the basic footer.  Will need to add a navigation section, a contact me section

--- cat-house-percussion/src/components/Footer.js
+++ cat-house-percussion/src/components/Footer.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
-import { Carousel } from 'react-bootstrap';
-import { Row } from 'react-flexbox-grid';
+import { footer } from 'react-bootstrap';
+import { Row, Col } from 'react-flexbox-grid';
 import '../App.css'
 
 class Footer extends Component {
   render() {
     return(
-      <div>
-        
+      <div className='Footer'>
+        <Row middle='xs'>
+          <Col xs>
+            <h4>Created by Eric Walz for his portfolio, 2018.</h4>
+          </Col>
+          <Col xs>
+            <h4>See his resume at ADD LINK HERE.</h4>
+          </Col>
+        </Row>
       </div>
     )
   }
 }
+
+export default Footer;
